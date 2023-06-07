c8c2987f6e57207d5662dc266b30290d5754f295
krishkalal <kalalkrishnakant@gmail.com>
2023-06-07 14:48:14 +0530

    update

--- customerapi/src/main/java/com/boa/customerapi/CustomerapiApplication.java
+++ customerapi/src/main/java/com/boa/customerapi/CustomerapiApplication.java
@@ -2,8 +2,10 @@ package com.boa.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerapiApplication {
 
     public static void main(String[] args) {
