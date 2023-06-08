f7f4127d38a29fd0286f2ceedea68e0f2580c3e9
krishkalal <kalalkrishnakant@gmail.com>
2023-06-08 14:13:37 +0530

    update

--- zuulgateway/src/main/java/com/boa/zuulgateway/ZuulgatewayApplication.java
+++ zuulgateway/src/main/java/com/boa/zuulgateway/ZuulgatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -13,5 +15,8 @@ public class ZuulgatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulgatewayApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 }
